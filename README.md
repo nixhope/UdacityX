b8f53a980bbe748e686c89a47a77753e30b4a7a8
Jin Koo <darksentinel@gmail.com>
2012-07-26 22:58:37 +1200

    Initial commit.
    Signed-off-by: Jin Koo <darksentinel@gmail.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+## Readme
+#What is Udacity Plus?
+
+Good question, Neo. Udacity Plus is a third party Grease Monkey userscript plugin that provides extra functionality for the MOOC website Udacity.
+This repository includes both the javascript file (tucked away in static/udacityplus.user.js) and the Python server code.
+
+##What kind of extra functionality?
+
+Currently it offers an article link at the top of the page where you can read student articles. This feature will soon be enhanced for greater usability. There's a screenshot hidden away in static/udacity_plus_screenshot.png.
+
+##That's it?
+
+Well, it's the future that's interesting. There will be extra tabs with bonus material for select lecture videos. And hopefully a space to take your own notes. There will also be extra buttons so you can nominate forum posts to be added to the article collection.
+
+Home website: [https://udacityplus.appspot.com/main][1]
+
+  [1]: https://udacityplus.appspot.com/main
---
+++ handler.py
@@ -0,0 +1,48 @@
+import webapp2
+from google.appengine.ext import db
+import logging
+import os
+import jinja2
+
+class Handler(webapp2.RequestHandler):
+    template_dir = os.path.join(
+            os.path.dirname(__file__), 'templates')
+    jinja_env = jinja2.Environment(
+        loader = jinja2.FileSystemLoader(template_dir),
+        autoescape = True)
+    
+    def write(self, *a, **kw):
+        '''Writes directly to the http request'''
+        logging.debug("%s: %s\n%s" % ('ip', self.request.remote_addr, self.request))
+        self.response.out.write(*a, **kw)
+        
+    def render_str(self, template, **params):
+        '''Returns a string of the rendered template'''
+        t = self.jinja_env.get_template(template)
+        return t.render(params)
+        
+    def render(self, template, **kw):
+        '''Render using the template and parameters'''
+        self.write(self.render_str(template, _url=self.request.url, **kw))
+        
+    def throw_error(self, code):
+        '''Throws an error, e.g. 404'''
+        self.response.set_status(code)
+        message = 'An error was encountered. Terribly sorry.'
+        if code == 403:
+            message = 'Access denied. You do not have permission to access this resource.'\
+            ' Please do not try again'
+        if code == 404:
+            message = 'The page or resource you requested could not be found. '\
+            'If you caught a broken link, please report it to the naughty admin.'
+        self.render('error.html', errorcode=code, errormessage=message)
+
+class ErrorHandler(Handler):
+    ''''''
+    def get(self, params):
+        self.throw_error(404)		
+        
+class SlashRedirect(Handler):
+    '''Removes multiple trailing slashes and redirects'''
+    def get(self, path):
+        self.redirect(path)
